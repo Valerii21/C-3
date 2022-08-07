74ad308500b9c9793f277831fdb8035afab891a5
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-08-07 13:29:33 +0300

    3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿ int num;
+  Console.WriteLine("Введите число:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 1)
+            {
+                Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+            }
+            else
+            {
+                Console.WriteLine("Число " + num + "является: ЧЁТНЫМ");
+            }
